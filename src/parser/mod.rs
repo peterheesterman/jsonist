e1655d47098698bc75169ac2bfca02124c081c10
Peter Heesterman <heesterman.peter@gmail.com>
2019-10-11 18:20:50 +0530

    feat(parser): consume objects

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -172,43 +172,43 @@ mod tests {
         }
     }
 
-    #[test]
-    fn object() {
-        // let json = r#" { "w in" : true }  "#;
+    // #[test]
+    // fn object() {
+    //     // let json = r#" { "w in" : true }  "#;
 
-        let whitespace = Token::WhiteSpace(0, ' ');
-        let open_brace = Token::OpenBrace(1);
-        let whitespace2 = Token::WhiteSpace(2, ' ');
-        let win = Token::StringLiteral(3, String::from("w in"));
-        let whitespace3 = Token::WhiteSpace(9, ' ');
-        let colon = Token::Colon(10);
-        let whitespace4 = Token::WhiteSpace(11, ' ');
-        let true_token = Token::True(12, "true");
-        let whitespace5 = Token::WhiteSpace(16, ' ');
-        let close_brace = Token::CloseBrace(17);
-        let whitespace6 = Token::WhiteSpace(18, ' ');
-        let whitespace7 = Token::WhiteSpace(19, ' ');
+    //     let whitespace = Token::WhiteSpace(0, ' ');
+    //     let open_brace = Token::OpenBrace(1);
+    //     let whitespace2 = Token::WhiteSpace(2, ' ');
+    //     let win = Token::StringLiteral(3, String::from("w in"));
+    //     let whitespace3 = Token::WhiteSpace(9, ' ');
+    //     let colon = Token::Colon(10);
+    //     let whitespace4 = Token::WhiteSpace(11, ' ');
+    //     let true_token = Token::True(12, "true");
+    //     let whitespace5 = Token::WhiteSpace(16, ' ');
+    //     let close_brace = Token::CloseBrace(17);
+    //     let whitespace6 = Token::WhiteSpace(18, ' ');
+    //     let whitespace7 = Token::WhiteSpace(19, ' ');
 
-        let tokens = vec![
-            whitespace,
-            open_brace,
-            whitespace2,
-            win,
-            whitespace3,
-            colon,
-            whitespace4,
-            true_token,
-            whitespace5,
-            close_brace,
-            whitespace6,
-            whitespace7,
-        ];
+    //     let tokens = vec![
+    //         whitespace,
+    //         open_brace,
+    //         whitespace2,
+    //         win,
+    //         whitespace3,
+    //         colon,
+    //         whitespace4,
+    //         true_token,
+    //         whitespace5,
+    //         close_brace,
+    //         whitespace6,
+    //         whitespace7,
+    //     ];
 
-        let ast = AST { root: Node::Object { pairs: vec![] } };
+    //     let ast = AST { root: Node::Object { pairs: vec![] } };
 
-        match parse(tokens) {
-            Ok(result) => assert_eq!(result, ast),
-            Err(e) => panic!("{}", e),
-        }
-    }
+    //     match parse(tokens) {
+    //         Ok(result) => assert_eq!(result, ast),
+    //         Err(e) => panic!("{}", e),
+    //     }
+    // }
 }
--- src/parser/object.rs
+++ src/parser/object.rs
@@ -3,13 +3,83 @@ use crate::tokenizer::Token;
 
 use super::Node;
 use super::JumpNode;
+use super::parse_node;
 
 pub fn parse_object(tokens: &Vec<Token>, position: usize) -> Result<JumpNode, FormatterError> {
-    let pairs = vec![];
-    let mut jump = 0;
-    jump = jump + 1;
+    let mut pairs = vec![];
+    let mut jump = position;
 
-    // Fill up the pairs
+    loop {
+        if let Some(token) = tokens.get(jump) {
+                println!("{:?}", token);
+            match token {
+                Token::CloseBrace(_) => return Ok((jump, Node::Object { pairs })),
+                Token::Comma(_) => {
+                    jump = jump + 1;
+                },
+                _ => {
+                    let (movement, key) = parse_node(&tokens, jump)?;
+                    jump = jump + movement + 1;
 
-    Ok((jump, Node::Object { pairs }))
+                    // TODO: check for a colon
+                    let (movement, value) = parse_node(&tokens, jump)?;
+                    jump = jump + movement;
+
+                    pairs.push(Box::new(Node::Pair { key: Box::new(key), value: Box::new(value) }))
+                }
+            }
+        } else {
+            return Err(FormatterError::ExpectedMoreCharacters(2))
+        }
+    }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::super::AST;
+    use super::*;
+
+    #[test]
+    fn parse_object_empty() {
+        let open_brace = Token::OpenBrace(0);
+        let close_brace = Token::CloseBrace(1);
+        let node = Node::Object { pairs: vec![] };
+
+        match parse_object(&vec![ open_brace, close_brace ], 1) {
+            Ok((_, result)) => assert_eq!(result, node),
+            Err(e) => panic!("{}", e),
+        }
+    }
+
+    #[test]
+    fn parse_object_one_pair() {
+        // let json = r#" { "w in" : true }  "#;
+
+        let open_brace = Token::OpenBrace(1);
+        let win = Token::StringLiteral(3, String::from("w in"));
+        let colon = Token::Colon(10);
+        let true_token = Token::True(12, "true");
+        let close_brace = Token::CloseBrace(17);
+
+        let tokens = vec![
+            open_brace,
+            win,
+            colon,
+            true_token,
+            close_brace,
+        ];
+
+        let node = Node::Object { pairs: vec![
+            Box::new(Node::Pair {
+                key: Box::new(Node::Literal { literal: String::from("w in") }),
+                value: Box::new(Node::True)
+            })
+        ]};
+
+        match parse_object(&tokens, 1) {
+            Ok((_, result)) => assert_eq!(result, node),
+            Err(e) => panic!("{}", e),
+        }
+    }
+}
+
