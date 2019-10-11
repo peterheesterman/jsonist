c86ceff2f2985b241cb064711b644d3d26b4d7b3
Peter Heesterman <heesterman.peter@gmail.com>
2019-10-11 12:45:27 +0530

    feat(tokenizer): get_next_token null, false and true,ls error has correct position for non-termination errors

--- src/tokenizer/get_next_token/collate.rs
+++ src/tokenizer/get_next_token/collate.rs
@@ -15,6 +15,7 @@ pub fn process_expectation(
     indexed_characters: IndexedCharacters,
 ) -> Result<Token, FormatterError> {
     let mut indexed_characters = indexed_characters;
+    let mut character_position = indexed_characters.get_index();
 
     for expected_character in literal.chars() {
         if let Some(&character) = indexed_characters.current_character() {
@@ -27,10 +28,11 @@ pub fn process_expectation(
                 });
             }
         } else {
-            return Err(FormatterError::ExpectedMoreCharacters(243243));
+            return Err(FormatterError::ExpectedMoreCharacters(character_position));
         };
 
         indexed_characters = indexed_characters.progress();
+        character_position = character_position + 1;
     }
 
     Ok(token)
@@ -102,7 +104,21 @@ mod tests {
     }
 
     #[test]
-    fn collagte_a_string_literal () {
+    #[should_panic(expected = "Expected more tokens a position 3")]
+    fn expect_failure_when_json_runs_out () {
+        let json = "fal";
+        let chars = json.chars().collect::<Vec<char>>();
+        let indexed_characters = IndexedCharacters::new(&chars);
+        let token = Token::StringLiteral(0, String::from("false"));
+        let token2 = Token::StringLiteral(0, String::from("false"));
+        match process_expectation("false", token, indexed_characters) {
+            Ok(result) => assert_eq!(result, token2),
+            Err(e) => panic!("{}", e),
+        }
+    }
+
+    #[test]
+    fn string_literal () {
         let json = "\"tester\"";
         let chars = json.chars().collect::<Vec<char>>();
         let indexed_characters = IndexedCharacters::new(&chars);
@@ -114,7 +130,7 @@ mod tests {
     }
 
     #[test]
-    fn collagte_a_string_literal_with_an_escape_in_it () {
+    fn string_literal_with_an_escape_in_it () {
         let json = r#""tes\"ter""#;
         let chars = json.chars().collect::<Vec<char>>();
         let indexed_characters = IndexedCharacters::new(&chars);
@@ -126,7 +142,7 @@ mod tests {
     }
 
     #[test]
-    fn collagte_a_string_literal_with_lots_of_escaped_quotes_in_it () {
+    fn string_literal_with_lots_of_escaped_quotes_in_it () {
         let json = r#""tes\"te\"   \"r""#;
         let chars = json.chars().collect::<Vec<char>>();
         let indexed_characters = IndexedCharacters::new(&chars);
