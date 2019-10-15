a80084e54842c21faa8ad25b1bde6fdd01a2d9a5
Peter Heesterman <heesterman.peter@gmail.com>
2019-10-15 13:35:42 +0530

    test(formatter): more nested input

--- tests/can_parse_correct_json.rs
+++ tests/can_parse_correct_json.rs
@@ -5,7 +5,7 @@ use std::fs;
 use jsonist::lint;
 
 #[test]
-fn integration_test() {
+fn integration_test_max_depth_one() {
     // TODO: depth and comma placement still need addressing in the output of this test
     let json = r#"
         {
@@ -20,7 +20,68 @@ fn integration_test() {
         }
     "#.to_owned();
 
-    let expected_out_file_path = "./tests/output/con_parse_corrrect_json.json";
+    let expected_out_file_path = "./tests/output/con_parse_correct_json.json";
+
+    let expected_contents = fs::read_to_string(expected_out_file_path).expect("No output file");
+
+    match lint(json) {
+        Ok(value) => assert_eq!(value, expected_contents),
+        Err(e) => panic!("{}", e)
+    }
+}
+
+#[test]
+fn integration_test_more_depth() {
+    // TODO: depth and comma placement still need addressing in the output of this test
+    let json = r#"
+{
+	"destination_addresses": [
+		"Washington, DC, USA",
+		"Philadelphia, PA, USA",
+		"Santa Barbara, CA, USA",
+		"Miami, FL, USA",
+		"Austin, TX, USA",
+		"Napa County, CA, USA"
+	],
+	"origin_addresses": [ "New York, NY, USA" ],
+	"rows": [{
+		"elements": [{
+                                "distance": { "text": "227 mi", "value": 365468 },
+				"duration": { "text": "3 hours 54 mins", "value": 14064 },
+				"status": "OK"
+			},
+			{
+				"distance": { "text": "94.6 mi", "value": 152193 },
+				"duration": { "text": "1 hour 44 mins", "value": 6227 },
+				"status": "OK"
+			},
+			{
+				"distance": { "text": "2,878 mi", "value": 4632197 },
+				"duration": { "text": "1 day 18 hours", "value": 151772 },
+				"status": "OK"
+			},
+			{
+				"distance": { "text": "1,286 mi", "value": 2069031 },
+				"duration": { "text": "18 hours 43 mins", "value": 67405 },
+				"status": "OK"
+			},
+			{
+				"distance": { "text": "1,742 mi", "value": 2802972 },
+				"duration": { "text": "1 day 2 hours", "value": 93070 },
+				"status": "OK"
+			},
+			{
+				"distance": { "text": "2,871 mi", "value": 4620514 },
+				"duration": { "text": "1 day 18 hours", "value": 152913 },
+				"status": "OK"
+			}
+		]
+	}],
+	"status": "OK"
+}
+    "#.to_owned();
+
+    let expected_out_file_path = "./tests/output/con_parse_complex_json.json";
 
     let expected_contents = fs::read_to_string(expected_out_file_path).expect("No output file");
 
